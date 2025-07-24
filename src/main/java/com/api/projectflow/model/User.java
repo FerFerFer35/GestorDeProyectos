a8ed08bde11a7797638b8810838a2dda11adbb89
FerFerFer35 <t20030435@itsch.edu.mx>
2025-07-24 17:12:42 -0600

    first commit

---
+++ src/main/java/com/api/projectflow/model/User.java
@@ -0,0 +1,27 @@
+package com.api.projectflow.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String correo;
+
+    @Column(nullable = false)
+    private String contrasena;
+}
---
+++ src/test/java/com/api/projectflow/ProjectflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.projectflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
